5682f6f3ee7b0c6db913bf63c1e1a7397dacb0cd
Tiago Vieira <tiagosvieira10@gmail.com>
2023-05-19 12:40:56 -0300

    projeto spacetime backend

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
+
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('servidor rodando')
+  })
